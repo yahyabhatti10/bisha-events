96ef7564fa9471ea11eec63df252470828373c79
Muhammad Yahya <yahyabhatti019@gmail.com>
2025-02-14 01:05:50 +0500

    reduced image size in hero section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
-import heroBackground from '../assets/home/hero-background.png';
+import heroBackground from '../assets/home/hero-background.jpg';
 
 const Home = () => {
   const navigate = useNavigate();
